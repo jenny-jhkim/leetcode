ff3c447ad25413c92c0edbfc983bcfe536bd2cff
jenny <133937601+jenny-jhkim@users.noreply.github.com>
2024-01-10 22:06:41 -0500

    Time: 34 ms (95.57%), Space: 17.4 MB (8.52%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,6 +1,7 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int):
         while n > 0:
+            #Compare the values of the last index
             if m > 0 and nums1[m-1] >= nums2[n-1]:
                 nums1[m+n-1] = nums1[m-1]
                 m -= 1
