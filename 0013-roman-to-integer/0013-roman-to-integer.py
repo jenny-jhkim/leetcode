b13bf2c475afe3be15c3c345878fdfe54f45f776
jenny <133937601+jenny-jhkim@users.noreply.github.com>
2024-03-15 21:13:47 -0400

    Time: 36 ms (95.30%), Space: 16.6 MB (72.39%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,26 +1,25 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        romanArray = ["I", "V", "X", "L", "C", "D", "M"]
-        intArray = [1, 5, 10, 50, 100, 500, 1000]
-        i = j = output = 0
+        romanSymbols = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        
+        i = output = 0
                 
         while i < len(s):
-            j = romanArray.index(s[i])  
-            print(i, j, s[i])
-            if i != len(s)-1 and (s[i] == "I" or s[i] == "X" or s[i] == "C"):
-                    #not last index
-                    nextRoman = s[i+1]        
-                    if nextRoman == romanArray[j+1] or nextRoman == romanArray[j+2]:
-                        k = romanArray.index(nextRoman)
-                        output += intArray[k] - intArray[j]
-                        i += 2
-                    else:
-                        output += intArray[j]
-                        i += 1
+            if i + 1 < len(s) and romanSymbols[s[i]] < romanSymbols[s[i+1]]:
+            #not last index
+                output += romanSymbols[s[i+1]] - romanSymbols[s[i]]
+                i += 2
             else :
-                output += intArray[j]
+                output += romanSymbols[s[i]]
                 i += 1
-            print(output)
             
         return output
         
