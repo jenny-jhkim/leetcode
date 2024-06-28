365f9de0811a2da9a581f0f9f4f55a01faf2321f
jenny <133937601+jenny-jhkim@users.noreply.github.com>
2024-06-27 21:06:04 -0400

    Time: 53 ms (87.44%), Space: 17.7 MB (29.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #using Hash table, Time complexity & Space complexity: O(n)
+        index_dict = {}
         
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return list([i,j])
+            check = target - nums[i]
+            
+            if check in index_dict:
+                return [i, index_dict[check]]
+            else:
+                index_dict[nums[i]] = i
+            
+        #time complexity : O(n^2), Space complexity: O(1)
+        #for i in range(len(nums)):
+            #for j in range(i+1, len(nums)):
+                #if nums[i] + nums[j] == target:
+                    #return list([i,j])
+                    #return [i,j]
+            
                     
             
             
