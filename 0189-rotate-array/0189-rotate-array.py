77ca337e24b1f14c9982d541fc8cb8d956816798
jenny <133937601+jenny-jhkim@users.noreply.github.com>
2024-07-29 11:14:10 -0400

    Time: 147 ms (92.19%), Space: 28 MB (52.44%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,8 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(k):
-            nums.insert(0, nums[-1])
-            nums.pop()
+        #Solution1
+        #for i in range(k):
+        #    nums.insert(0, nums[-1])
+        #    nums.pop()
 
-        
+        #Solution2
+        n = len(nums)
+        k %= n #if k is greater than n
+        nums_part1 = nums[0:n-k]
+        nums_part2 = nums[n-k:]
+
+        nums[:] = nums_part2 + nums_part1 #needs to update original list
