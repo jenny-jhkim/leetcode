e88c48860a6fdaad194bf8df03b489bdf26a44bd
jenny <133937601+jenny-jhkim@users.noreply.github.com>
2024-07-29 13:11:44 -0400

    Time: 82 ms (5.37%), Space: 16.8 MB (11.53%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -6,14 +6,13 @@ class Solution:
             print("i:", i, nums[i], nums[i+1])
             if nums[i] != nums[i+1] :
                 i+=1
-                count = 0
             else:
-                count+=1
-                if count >=2:
+                if i < len(nums)-2 and nums[i] == nums [i+2]:
                     nums[i+1:]=nums[i+2:]
                     print(nums)
                 else:
                     i+=1
+
         print(nums)
         k = len(nums)
         return k
