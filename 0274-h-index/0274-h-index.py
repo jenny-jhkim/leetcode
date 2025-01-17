b6d3d2ee38a0da08512b3bd32b44cdf98046a711
jenny <133937601+jenny-jhkim@users.noreply.github.com>
2025-01-17 12:17:42 -0500

    Time: 0 ms (100%), Space: 18 MB (33.83%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -4,7 +4,7 @@ class Solution:
         hIndex = 0
 
         for i, c in enumerate(citations):
-            if i+1 <= c:
+            if i+1 <= c: #compare papers and current citations[i]
                 hIndex += 1
             else:
                 break
